4e9d3dc1d80cb5d610f4cbb225ffbd6df5890676
Marina Poltorak <emylisfamily@gmail.com>
2019-04-26 12:55:02 -0700

    testing

--- WordCounter.Tests/ModelTests/WordTests.cs
+++ WordCounter.Tests/ModelTests/WordTests.cs
@@ -11,50 +11,50 @@ namespace WordCounter.Tests
     [TestMethod]
     public void WordConstructor_CreatesInstanceOfWord_Word()
     {
-      Word newWord = new Word();
+      Word newWord = new Word("test sentence", "testWord", 0);
       Assert.AreEqual(typeof(Word), newWord.GetType());
     }
     [TestMethod]
     public void GetSentence_ChecksGets_Sentence()
     {
       string sent = "test sentence";
-      Word newWord = new Word(sent);
+      Word newWord = new Word(sent, "placeHolder", 0);
       Assert.AreEqual(sent, newWord.GetSent());
     }
   }
 }
-    // [TestMethod]
-    //     public void GetWord_ChecksGets_Word()
-    //     {
-    //         string testWord = "testWord";
-    //         Word newWord = new Word("place Hold", testWord);
-    //         Assert.AreEqual(testWord, newWord.GetWord());
-    //     }
-    //
-    //     [TestMethod]
-    //     public void WordFind_FindsWords_2()
-    //     {
-    //         string testWord = "apple";
-    //         string testSent = "There is an apple in the apple tree, not applejam";
-    //         Word newWord = new Word(testSent, testWord);
-    //         Assert.AreEqual(2, newWord.WordFinder());
+    [TestMethod]
+        public void GetWord_ChecksGets_Word()
+        {
+            string testWord = "testWord";
+            Word newWord = new Word("place Hold", testWord, 0);
+            Assert.AreEqual(testWord, newWord.GetWord());
+        }
 
-    // [TestMethod]
-    // public void SetDescription_SetDescription_String()
-    // {
-    //   string description = "One apple on the apple tree";
-    //   RepeatCounter newRepeatCounter = new RepeatCounter(description);
-    //   string updatedDescription = "Just pears on the apple tree";
-    //   newRepeatCounter.SetDescription(updatedDescription);
-    //   string result = newRepeatCounter.GetDescription();
-    //   Assert.AreEqual(updatedDescription, result);
-    // }
-    // [TestMethod]
-    // public void GetAll_ReturnsEmptyList_RepeatCounterList()
-    // {
-    //   List<RepeatCounter> newList = new List<RepeatCounter> { };
-    //   List<RepeatCounter> result = RepeatCounter.GetAll();
-    //   CollectionAssert.AreEqual(newList, result);
+        [TestMethod]
+        public void WordFind_FindsWords_2()
+        {
+            string testWord = "apple";
+            string testSent = "There is an apple in the apple tree, not applejam";
+            Word newWord = new Word(testSent, testWord, 0);
+            Assert.AreEqual(2, newWord.WordFinder());
+        }
+        [TestMethod]
+        public void FindWord_ChecksForSpace_false()
+        {
+            string testWord = "applepie";
+            string testSent = "Apple is great for apple pie";
+            Word newWord = new Word(testSent, testWord, 0);
+            Assert.AreEqual(false, newWord.CheckWord());
+        }
+        [TestMethod]
+        public void FindWord_ChecksForSpace_true()
+        {
+            string testWord = "applepie";
+            string testSent = "Apple is great for apple pie";
+            Word newWord = new Word(testSent, testWord, 0);
+            Assert.AreEqual(true, newWord.CheckWord());
+
 //     }
 //   }
 // }
