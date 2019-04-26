36cc68bff39dbc439763f3915f7a58609df5f7d4
Marina Poltorak <emylisfamily@gmail.com>
2019-04-26 14:23:25 -0700

    make five out of six tests work

--- WordCounter.Tests/ModelTests/WordTests.cs
+++ WordCounter.Tests/ModelTests/WordTests.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace WordCounter.Tests
 {
   [TestClass]
-  public class Word
+  public class WordTest
   {
     [TestMethod]
     public void WordConstructor_CreatesInstanceOfWord_Word()
@@ -14,47 +14,46 @@ namespace WordCounter.Tests
       Word newWord = new Word("test sentence", "testWord", 0);
       Assert.AreEqual(typeof(Word), newWord.GetType());
     }
+
     [TestMethod]
     public void GetSentence_ChecksGets_Sentence()
     {
-      string sent = "test sentence";
-      Word newWord = new Word(sent, "placeHolder", 0);
-      Assert.AreEqual(sent, newWord.GetSent());
+      string sentence = "test sentence";
+      Word newWord = new Word(sentence, "placeHolder", 0);
+      Assert.AreEqual(sentence, newWord.GetSentence());
+    }
+
+    [TestMethod]
+    public void GetWord_ChecksGets_Word()
+    {
+      string testWord = "testWord";
+      Word newWord = new Word("place Holder", testWord, 0);
+      Assert.AreEqual(testWord, newWord.GetWord());
+    }
+
+    [TestMethod]
+    public void WordFind_FindsWords_2()
+    {
+      string testWord = "apple";
+      string testSentence = "There is an apple in the apple tree, not applejam";
+      Word newWord = new Word(testSentence, testWord, 0);
+      Assert.AreEqual(2, newWord.WordFinder());
+    }
+    [TestMethod]
+    public void FindWord_ChecksForSpace_false()
+    {
+      string testWord = "applepie";
+      string testSentence = "Apple is great for apple pie";
+      Word newWord = new Word(testSentence, testWord, 0);
+      Assert.AreEqual(false, newWord.FindWord());
+    }
+    [TestMethod]
+    public void FindWord_ChecksForSpace_true()
+    {
+      string testWord = "applepie";
+      string testSentence = "Apple is great for apple pie";
+      Word newWord = new Word(testSentence, testWord, 0);
+      Assert.AreEqual(true, newWord.FindWord());
     }
   }
 }
-    [TestMethod]
-        public void GetWord_ChecksGets_Word()
-        {
-            string testWord = "testWord";
-            Word newWord = new Word("place Hold", testWord, 0);
-            Assert.AreEqual(testWord, newWord.GetWord());
-        }
-
-        [TestMethod]
-        public void WordFind_FindsWords_2()
-        {
-            string testWord = "apple";
-            string testSent = "There is an apple in the apple tree, not applejam";
-            Word newWord = new Word(testSent, testWord, 0);
-            Assert.AreEqual(2, newWord.WordFinder());
-        }
-        [TestMethod]
-        public void FindWord_ChecksForSpace_false()
-        {
-            string testWord = "applepie";
-            string testSent = "Apple is great for apple pie";
-            Word newWord = new Word(testSent, testWord, 0);
-            Assert.AreEqual(false, newWord.CheckWord());
-        }
-        [TestMethod]
-        public void FindWord_ChecksForSpace_true()
-        {
-            string testWord = "applepie";
-            string testSent = "Apple is great for apple pie";
-            Word newWord = new Word(testSent, testWord, 0);
-            Assert.AreEqual(true, newWord.CheckWord());
-
-//     }
-//   }
-// }
